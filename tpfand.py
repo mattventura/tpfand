3e9c9e26d0b4563b451636ada3861bdc72a9f304
Matt Ventura <mattventura@mattventura.net>
2016-05-31 10:30:16 -0700

    Partial rewrite

--- tpfand.py
+++ tpfand.py
@@ -2,19 +2,22 @@
 
 sensor1 = '/sys/devices/platform/coretemp.0/hwmon/hwmon1/temp2_input'
 sensor2 = '/sys/devices/platform/coretemp.0/hwmon/hwmon1/temp3_input'
-fanfile = '/proc/acpi/ibm/fan'
+primary_fanfile = '/proc/acpi/ibm/fan'
 
 import time
 
 
 class fanOut(object):
-    def setactive(self):
+
+    def __init__(self, fanfile):
+        self.fanfile = fanfile
+
+    def set_level(self, level):
         with open(self.fanfile, 'w') as f:
-            f.write(self.output)
+            f.write(level.outtext)
 
 
-class defaultFanOut(fanOut):
-    fanfile = fanfile
+defaultFanOut = fanOut(primary_fanfile)
 
 
 class tempSensor(object):
@@ -22,7 +25,7 @@ class tempSensor(object):
     def __init__(self, sensorFiles):
         self.sensorFiles = sensorFiles
 
-    def gettemp(self):
+    def get_temp(self):
         maxtemp = None
         for sensorpath in self.sensorFiles:
             with open(sensorpath, 'r') as f:
@@ -35,48 +38,35 @@ class tempSensor(object):
 defaultTempSensor = tempSensor([sensor1, sensor2])
 
 
-class fanLevel(defaultFanOut):
-    def __init__(self, output, mintemp, maxtemp):
+class fanLevel(object):
+    def __init__(self, outtext, mintemp, maxtemp):
         # Validate
         if mintemp is not None is not maxtemp:
             if mintemp > maxtemp:
                 raise Exception('Lower max than min')
-        self.output = output
+        self.outtext = outtext
         self.mintemp = mintemp
         self.maxtemp = maxtemp
-        self.isactive = False
 
-    def markactive(self):
-        self.isactive = True
-
-    def markinactive(self):
-        self.isactive = False
-
-    def setactive(self):
-        super(self.__class__, self).setactive()
-        self.markactive()
-
-    def reapply(self):
-        super(self.__class__, self).setactive()
-
-    # Going to be lazy and use these
     def isTooHigh(self, temp):
         if self.maxtemp is None:
             return False
         return temp > self.maxtemp
 
     def isTooLow(self, temp):
+        if self.mintemp is None:
+            return False
         return temp < self.mintemp
 
 
 class fanController(object):
-    def __init__(self, fanLevels, default=0):
-        assert len(fanLevels) > 0
+    def __init__(self, fanOut, fanLevels, default=0):
+        assert len(fanLevels)
         validateFanLevels(fanLevels)
+        self.fanOut = fanOut
         self.fanLevels = fanLevels
         self.curIdx = default
         self.maxIdx = len(fanLevels) - 1
-        self.curLevel.setactive()
 
     @property
     def curLevel(self):
@@ -85,28 +75,22 @@ class fanController(object):
     def getLevel(self, idx):
         return self.fanLevels[idx]
 
-    def process(self, temp):
+    def find_desired_level(self, temp):
         curLevel = self.curLevel
         if curLevel.isTooHigh(temp) and self.curIdx < self.maxIdx:
             self.curIdx += 1
-            self.process(temp)
+            self.find_desired_level(temp)
 
         elif curLevel.isTooLow(temp) and self.curIdx > 0:
             self.curIdx -= 1
-            self.process(temp)
+            self.find_desired_level(temp)
 
     def update(self, temp):
-        oldIdx = self.curIdx
-        self.process(temp)
-        newIdx = self.curIdx
+        self.find_desired_level(temp)
+        self.apply_level(self.curLevel)
 
-        if oldIdx == newIdx:
-            self.curLevel.reapply()
-
-        else:
-            oldLevel = self.getLevel(oldIdx)
-            oldLevel.markinactive()
-            self.curLevel.setactive()
+    def apply_level(self, level):
+        self.fanOut.set_level(level)
 
 
 class fanLoop(object):
@@ -133,14 +117,14 @@ def validateFanLevels(fanLevels):
 
 fanlevels = [
     fanLevel('disable', None, 55),
-    fanLevel('level 3', 50, 70),
+    fanLevel('level 3', 45, 70),
     fanLevel('level 5', 65, 85),
     fanLevel('level 7', 75, None),
 ]
 
 
-mainFanController = fanController(fanlevels)
-mainFanLoop = fanLoop(mainFanController, defaultTempSensor.gettemp)
+mainFanController = fanController(defaultFanOut, fanlevels)
+mainFanLoop = fanLoop(mainFanController, defaultTempSensor.get_temp)
 
 while True:
     mainFanLoop.cycle()
